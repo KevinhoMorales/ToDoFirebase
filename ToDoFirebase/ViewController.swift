a592e3495b9b0fb7ce5af87d94e93033d5849fd0
Kevin Morales <kmorales@technisys.com>
2021-12-17 10:56:13 -0500

    Project init

---
+++ ToDoFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoFirebase
+//
+//  Created by Kevin Morales on 12/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
